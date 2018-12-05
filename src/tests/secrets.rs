7ed16d8f9e939706fa43709433e60b03f33a8aa7
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2018-12-04 21:33:07 -0600

    Add test retrieve after writing to disk and reloading

--- src/tests/secrets.rs
+++ src/tests/secrets.rs
@@ -12,9 +12,15 @@ fn basic_store_retrieve() {
     // make sure that we can set values in different &str/String types
     sman.set("foo", "bar");
     sman.set("foo", "bar".to_string());
-    sman.save();
+    sman.save().unwrap();
 
     // do we get the same value back on retrieve?
     let retrieved: String = sman.retrieve("foo").unwrap();
+    assert_eq!("bar", retrieved);
+
+    // Now open the store from the disk with the same settings and make sure the data remains
+    // loadable.
+    let sman2 = SecretsManager::load("./secrets.json", KeySource::Password("mysecret")).unwrap();
+    let retrieved: String = sman2.retrieve("foo").unwrap();
     assert_eq!("bar", retrieved);
 }
