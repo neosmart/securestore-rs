38f89c772eb66f4db1f2ef781bc16f6bce2d6651
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2022-07-17 13:15:24 -0500

    Add `BinarySerializable` impl for `Vec<u8>`
    It was previously covered by the impl for `&[u8]`, but it can't hurt to add a
    direct impl as well.

--- securestore/src/serial.rs
+++ securestore/src/serial.rs
@@ -1,35 +1,32 @@
-//! This module houses various implementations of [`BinarySerializable`] which
-//! are natively supported payloads for secrets. This is intentionally not
-//! implemented as a global impl for all `TryFrom<u8>`/`TryInto<u8>` as the
-//! binary conversion must be stable to survive cold storage.
+//! This module houses the [`BinarySerializable`] and [`BinaryDeserializable`]
+//! traits and implementations for the natively supported primitive secret
+//! types.
+//!
+//! This is intentionally not implemented as a global impl for all
+//! `TryFrom<&[u8]>`/`TryInto<Vec<u8>>` as the binary conversion must be stable
+//! to survive cold storage. Keep in mind compatibility with other SecureStore
+//! clients accessing or setting secrets in the same vault from
+//! different languages or frameworks when implementing these traits, if that is
+//! a concern.
 
 use crate::errors::StdError;
 
-/// A trait enabling saving a secret of the implementing type to the secrets
-/// store.
+/// A trait enabling directly saving a secret of the implementing type to the
+/// SecureStore vault.
+///
+/// `BinarySerializable` implementations for `String`, `&str`, `Vec<u8>`, and
+/// `&[u8]` are provided by this crate; implement this trait to support directly
+/// setting other types via calls to
+/// [`SecretsManager::set()`](crate::SecretsManager::set) but make sure to keep
+/// compatibility with other clients accessing the same store from different
+/// languages or frameworks in mind, if that is a concern.
 pub trait BinarySerializable {
     fn serialize<'a>(&'a self) -> &'a [u8];
-}
-
-/// A trait enabling retrieving a secret from the secrets store as an instance
-/// of the implementing type.
-pub trait BinaryDeserializable
-where
-    Self: Sized,
-{
-    fn deserialize(bytes: Vec<u8>) -> Result<Self, Box<StdError>>;
 }
 
 impl BinarySerializable for String {
     fn serialize<'a>(&'a self) -> &'a [u8] {
         self.as_bytes()
-    }
-}
-
-impl BinaryDeserializable for String {
-    fn deserialize(bytes: Vec<u8>) -> Result<Self, Box<StdError>> {
-        let s = String::from_utf8(bytes)?;
-        Ok(s)
     }
 }
 
@@ -39,9 +36,37 @@ impl BinarySerializable for &str {
     }
 }
 
+impl BinarySerializable for Vec<u8> {
+    fn serialize<'a>(&'a self) -> &'a [u8] {
+        self.as_slice()
+    }
+}
+
 impl BinarySerializable for &[u8] {
     fn serialize<'a>(&'a self) -> &'a [u8] {
         self
+    }
+}
+
+/// A trait enabling directly retrieving a secret from the SecureStore vault as
+/// an instance of the implementing type.
+///
+/// `BinaryDeserializable` implementations for `String` and `Vec<u8>` are
+/// provided by this crate; implement this trait to support directly retrieving
+/// other owned types via calls to
+/// [`SecretsManager::get()`](crate::SecretsManager::set) but make sure to keep
+/// compatibility with other clients accessing the same store from different
+/// languages or frameworks in mind, if that is a concern.
+pub trait BinaryDeserializable
+where
+    Self: Sized,
+{
+    fn deserialize(bytes: Vec<u8>) -> Result<Self, Box<StdError>>;
+}
+
+impl BinaryDeserializable for String {
+    fn deserialize(bytes: Vec<u8>) -> Result<Self, Box<StdError>> {
+        Ok(String::from_utf8(bytes)?)
     }
 }
 
